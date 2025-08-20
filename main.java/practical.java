fbf5b01a7c0dc164e2506332aaa63005caf18d01
himanshugurnule07 <himanshugurnule07@gmail.com>
2025-08-20 23:42:00 +0530

    Add files via upload

---
+++ main.java/practical.java
@@ -0,0 +1,16 @@
+// HelloWorld.java
+class HelloWorld {
+public static void main(String[] args) {
+// Printing Hello World
+System.out.println(&quot;Hello, World!&quot;);
+
+// Arithmetic operations
+int a = 10, b = 5;
+
+System.out.println(&quot;Addition: &quot; + (a + b));
+System.out.println(&quot;Subtraction: &quot; + (a - b));
+System.out.println(&quot;Multiplication: &quot; + (a * b));
+System.out.println(&quot;Division: &quot; + (a / b));
+System.out.println(&quot;Modulus: &quot; + (a % b));
+}
+}
